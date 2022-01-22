69de9b1191d319fe59924b82bdf024f7e82dccf2
bnaze <bavernaz@hotmail.com>
2022-01-22 19:56:08 +0000

    setup db service

---
+++ backgend/db-service.tsx
@@ -0,0 +1,62 @@
+import { openDatabase, SQLiteDatabase } from 'react-native-sqlite-storage';
+import exercisesJson from './exercises.json';
+
+export const getDBConnection = () => {
+  return openDatabase({ name: 'lifthouse-data.db', location: 'default' });
+};
+
+const buildValuesFromJson = (exerciseType: string[]) => {
+  let values = '';
+  exerciseType.map(exercise => {
+    values = values.concat(`("${exercise}"),`);
+  });
+  return values.substring(0, values.length - 1);
+};
+
+const populateExercises = (db: SQLiteDatabase) => {
+  const insertPullExercises =
+    'INSERT INTO exercises (exerciseName) VALUES ' +
+    buildValuesFromJson(exercisesJson.exercises.pull as string[]);
+
+  const insertPushExercises =
+    'INSERT INTO exercises (exerciseName) VALUES ' +
+    buildValuesFromJson(exercisesJson.exercises.push as string[]);
+
+  const insertLegExercises =
+    'INSERT INTO exercises (exerciseName) VALUES ' +
+    buildValuesFromJson(exercisesJson.exercises.legs as string[]);
+
+  db.executeSql(insertPullExercises);
+  db.executeSql(insertPushExercises);
+  db.executeSql(insertLegExercises);
+};
+
+/**
+ * Creates tables if doesn't exist
+ * @table routineToExercise This table is used to match routines with exercises
+ *        this is to match each routine with the exercises that users have selected
+ *        since SQLite doesn't support arrays, this intermediate table is needed
+ */
+const createTables = async (db: SQLiteDatabase) => {
+  const dropExercises = 'DROP TABLE IF EXISTS exercises;';
+
+  db.executeSql(dropExercises);
+
+  const exercises =
+    'CREATE TABLE IF NOT EXISTS exercises (exerciseId INTEGER PRIMARY KEY, exerciseName TEXT NOT NULL);';
+  const routines =
+    'CREATE TABLE IF NOT EXISTS routines (routineId INTEGER PRIMARY KEY, routineName TEXT NOT NULL);';
+  const routineToExercises =
+    'CREATE TABLE IF NOT EXISTS routineToExercises (routineId INTEGER REFERENCES routines(routineId), exerciseId INTEGER REFERENCES exercises(exerciseId));';
+  const entries =
+    'CREATE TABLE IF NOT EXISTS entries (entryId INTEGER PRIMARY KEY, exerciseId INTEGER REFERENCES exercises(exerciseId), date TEXT NOT NULL, setnum INTEGER, reps INTEGER, weight INTEGER);';
+
+  db.executeSql(exercises);
+  db.executeSql(routines);
+  db.executeSql(routineToExercises);
+  db.executeSql(entries);
+};
+
+const db = getDBConnection();
+createTables(db);
+populateExercises(db);
