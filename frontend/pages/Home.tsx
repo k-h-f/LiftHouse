3f2380ff5db46399c9e4ee3302b5b1804e03573e
bnaze <bavernaz@hotmail.com>
2022-01-26 21:18:25 +0000

    wip create routines

--- frontend/pages/Home.tsx
+++ frontend/pages/Home.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { ActivityIndicator, View } from 'react-native';
+import {
+  createStackNavigator,
+  CardStyleInterpolators,
+} from '@react-navigation/stack';
 import QueryAlias from '../../backend/queryAlias';
 import { Routine } from '../../backend/responseTypes.ts/getRoutines';
 import GlobalText from '../shared/components/GlobalText';
@@ -8,13 +12,15 @@ import PageStyle from '../shared/stylesheets/pages.style';
 import useDatabase from '../utils/useDatabase';
 import EmptyRoutine from './components/EmptyRoutine';
 import styles from './Home.style';
+import CreateRoutine from './CreateRoutine';
 
-const Home: React.FC = () => {
+const Stack = createStackNavigator();
+
+const HomeView: React.FC = () => {
   const { data, isCompleted } = useDatabase(QueryAlias.GET_ROUTINES);
   const routines = data as Routine[];
 
   const hasRoutines = isCompleted === true && routines.length !== 0;
-
   return (
     <View style={PageStyle.wrapper}>
       <View style={styles.header_wrapper}>
@@ -28,5 +34,17 @@ const Home: React.FC = () => {
     </View>
   );
 };
+
+const Home: React.FC = () => (
+  <Stack.Navigator
+    screenOptions={{
+      headerShown: false,
+      cardStyleInterpolator: CardStyleInterpolators.forVerticalIOS,
+    }}
+  >
+    <Stack.Screen name="Home" component={HomeView} />
+    <Stack.Screen name="CreateRoutine" component={CreateRoutine} />
+  </Stack.Navigator>
+);
 
 export default Home;
--- frontend/pages/components/EmptyRoutine.tsx
+++ frontend/pages/components/EmptyRoutine.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
 import { View } from 'react-native';
 import { Button } from 'react-native-paper';
+import { useNavigation } from '@react-navigation/native';
 import GlobalText from '../../shared/components/GlobalText';
 import { colors } from '../../themeConfig';
 import styles from './EmptyRoutine.style';
 
 const EmptyRoutine: React.FC = () => {
+  const navigation = useNavigation();
+
+  const createBtnOnPress = () => {
+    navigation.navigate({ name: 'CreateRoutine' });
+  };
+
   return (
     <View style={styles.wrapper}>
       <GlobalText style={styles.no_routine_message}>
         No Routines added. Start creating routines
       </GlobalText>
-      <Button testID="create-btn" color={colors.highlight}>
+      <Button
+        testID="create-btn"
+        color={colors.highlight}
+        onPress={() => createBtnOnPress()}
+      >
         CREATE
       </Button>
     </View>
